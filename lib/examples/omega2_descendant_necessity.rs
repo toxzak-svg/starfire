ccdadf976fc99dc3683a18180d93eb3d6c220146
Zachary Maronek <zwmaronek@gmail.com>
2026-07-11 07:02:22 -0400

    experiment(cognition): expose Ω2 descendant necessity executable

---
+++ lib/examples/omega2_descendant_necessity.rs
@@ -0,0 +1,6 @@
+#![allow(dead_code)]
+
+include!("omega2_descendant_necessity/part1.rs");
+include!("omega2_descendant_necessity/part2.rs");
+include!("omega2_descendant_necessity/part3.rs");
+include!("omega2_descendant_necessity/part4.rs");
