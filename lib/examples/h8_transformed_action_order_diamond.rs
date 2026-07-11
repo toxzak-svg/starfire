1ea8d9194a382fca562cc46f03acbff67d3494f6
Zachary Maronek <zwmaronek@gmail.com>
2026-07-11 02:06:24 -0400

    experiment(charge): expose H8 transformed action-order diamond executable

---
+++ lib/examples/h8_transformed_action_order_diamond.rs
@@ -0,0 +1,4 @@
+include!("h8_transformed_action_order/part1.rs");
+include!("h8_transformed_action_order/part2.rs");
+include!("h8_transformed_action_order/part3.rs");
+include!("h8_transformed_action_order/part4.rs");
